ce05d264dfb70705af6417b46d4e6f70833b81e1
Anders Flotten <andersflotten@Anderss-MacBook-Air.local>
2023-02-01 18:19:35 -0700

    List schedule components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+
+  const displayAppointment = props.interview ? <Show {...props}/> : <Empty />;
+
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {displayAppointment}
+    </article>
   );
 };
